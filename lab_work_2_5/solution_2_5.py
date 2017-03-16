ded6095f9b2c85dfd6bb2523f899492e7f8e16c6
vitaliishpak <shpakvv@gmail.com>
2017-03-16 20:23:59 +0200

    Lab Work 2.4. added

---
+++ lab_work_2_5/solution_2_5.py
@@ -0,0 +1,40 @@
+"""create dict with 5 items, where keys will be country name and value - domain name, i.e. {Ukraine:UA}
+"""
+COUNT = {'France': 'FR', 'Spain': 'SP', 'Great Britain': 'GB', 'Italy': 'IT', 'Poland': 'PL',}
+print(COUNT)
+
+"""create another dict with 5 items, where values of countries will be keys, and values will be the capitals i.e. {'UA': 'Kiev'}
+"""
+CAP = {'FR': 'Paris', 'SP': 'Madrid', 'GB': 'London', 'IT': 'Rome', 'PL': 'Warsaw',}
+print(CAP)
+
+"""add one more element to each dict above
+"""
+COUNT.update(Ukraine='UA')
+print(COUNT)
+CAP.update(UA='Kyiv')
+print(CAP)
+
+"""print sentence "Domain for COUNTRY is DOMAIN." for each record in countries with the replace from dicts
+"""
+for key, value in COUNT.items():
+    print("Domain for {} is {}". format(key, value))
+
+"""print sentence "The capital of COUNTRY is CAPITAL" for each record in capitals with the replace from dicts
+"""
+for key, value in CAP.items():
+    print("The capital of {} is {}". format(key, value))
+
+"""Merge sentences above together with one cycle
+"""
+for key, value in COUNT.items():
+    print("Domain for {} is {}. The capital is {}"
+          .format(key, value, CAP[value]))
+
+"""Add to each value of first dict another two domains: COM and GOV
+"""
+for key, value in COUNT.items():
+    COUNT[key] = [value, 'COM', 'GOV']
+for key, value in COUNT.items():
+    print("Domain for {} is {}. The capital is {}"
+          .format(key, value, CAP[value[0]]))
